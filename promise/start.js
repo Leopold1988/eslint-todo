17bdd83186b8891a5c6075ef5fef06bcc5285f44
Peng Li <pengli@192.168.0.6>
2019-11-25 14:40:03 +0800

    push and eslint

--- promise/start.js
+++ promise/start.js
@@ -5,7 +5,7 @@ let p = new Promise((resolve, reject)=>{
 });
 
 p.then((data)=>{
-    console.log('success1', data);
+    console.log('success', data);
 },(err)=>{
     console.log('fail1', err);
 });
